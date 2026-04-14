d496b8b3ce348887cd6b86e8d2de08f4631a74a1
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2026-04-14 16:28:46 +0300

    Add modal Provider

--- front/app/layout.js
+++ front/app/layout.js
@@ -1,4 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { ModalProvider } from "@/Context/ModalContext";
+import Modal from "../components/Modal/Modal";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -19,7 +21,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+      <body>
+        <ModalProvider>
+          {children}
+          <Modal />
+        </ModalProvider>
+      </body>
     </html>
   );
 }
