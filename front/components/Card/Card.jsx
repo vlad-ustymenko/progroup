07343db348013783a396830fc55546d9a7c82a51
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2026-04-14 16:28:31 +0300

    Add button functionality

--- front/components/Card/Card.jsx
+++ front/components/Card/Card.jsx
@@ -1,9 +1,12 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import styles from "./Card.module.css";
 import { FaCirclePlus } from "react-icons/fa6";
+import { useModal } from "@/Context/ModalContext";
 
 const Card = ({ image, title, subtitle }) => {
+  const { openModal } = useModal();
   return (
     <div className={styles.card}>
       <Image
@@ -17,7 +20,17 @@ const Card = ({ image, title, subtitle }) => {
         <p className={styles.title}>{title}</p>
         <p className={styles.subtitle}>{subtitle}</p>
       </div>
-      <FaCirclePlus className={styles.button} />
+      <FaCirclePlus
+        className={styles.button}
+        onClick={() =>
+          openModal({
+            title,
+            description:
+              "Ми не просто шукаємо вільну ділянку — ми прогнозуємо розвиток району на 10 років вперед. Транспортна логіка: зручний доступ до ключових артерій міста без компромісів із тишею. Інфраструктурний потенціал: наявність садочків, шкіл та парків у кроковій доступності. Екологічний контекст: пріоритет ділянкам поблизу рекреаційних зон, де місто «дихає».",
+            image,
+          })
+        }
+      />
     </div>
   );
 };
