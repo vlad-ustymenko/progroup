ffdc0343ecb85cbb53d269e793cbaf5896df053e
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2026-04-16 18:50:29 +0300

    Delete viewTransition from nextconfig

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    viewTransition: true,
-  },
   images: {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
