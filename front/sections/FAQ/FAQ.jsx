dc581341874eaa77e079a097b0a320e2b436f32d
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2026-04-17 16:29:52 +0300

    Add strapi data in FAQ

--- front/sections/FAQ/FAQ.jsx
+++ front/sections/FAQ/FAQ.jsx
@@ -3,62 +3,77 @@ import React, { useState } from "react";
 import ArrowIcon from "@/components/ArrowIcon/ArrowIcon";
 import styles from "./FAQ.module.css";
 
-const FAQ = () => {
-  const [activeIndex, setActiveIndex] = useState(null);
+const FAQ = ({ data, categories }) => {
+  const [activeId, setActiveId] = useState(null);
+  const [activeCategory, setActiveCategory] = useState(
+    data.faq_list[0].faq_category.slug,
+  );
 
-  const toggleTab = (index) => {
-    // якщо клік по відкритому — закриваємо
-    if (activeIndex === index) {
-      setActiveIndex(null);
-    } else {
-      setActiveIndex(index);
-    }
+  const toggleTab = (id) => {
+    setActiveId(activeId === id ? null : id);
   };
 
-  const data = [
-    {
-      title: "Чи можна купити квартиру дистанційно?",
-      content:
-        "Так, ми забезпечуємо повний супровід віддалених угод для інвесторів із Закарпаття, інших регіонів чи з-за кордону. Оформлення документів можливе через електронний підпис (ЕЦП) або за нотаріальною довіреністю. Ми надаємо відеоогляди будівництва та онлайн-консультації на кожному етапі.",
-    },
-    {
-      title: "Як забронювати квартиру в новобудовах?",
-      content:
-        "Щоб забронювати житло в наших проєктах у Києві, передмісті або Ужгороді, достатньо залишити заявку на сайті. Після консультації ми підписуємо договір бронювання, який фіксує за вами обране планування та ціну на визначений термін. Це гарантує, що квартира не буде продана іншим покупцям, поки ви готуєте документи.",
-    },
-    {
-      title: "Які документи підтверджують надійність будівництва ProGroup? ",
-      content:
-        "Як девелопер із власним будівельним ресурсом, ми забезпечуємо повну юридичну прозорість. На кожен наш об'єкт у Києві та Закарпатті наявний повний пакет дозвільної документації: право на землю, дозвіл на будівництво та технічні умови на підключення комунікацій. Ознайомитися з документами можна у відділах продажу наших ЖК.",
-    },
-  ];
+  const handleFilterChange = (category) => {
+    setActiveCategory(category);
+    setActiveId(null); // закриваємо відкритий таб при зміні фільтра
+  };
+
+  const filteredData =
+    activeCategory === "all"
+      ? data
+      : data.faq_list.filter(
+          (item) => item.faq_category.slug === activeCategory,
+        );
+
+  console.log(data);
 
   return (
     <div className={styles.faq}>
-      <h2 className={styles.title}>Питання та відповіді (FAQ)</h2>
+      <h2 className={styles.title}>{data.sectionTitle}</h2>
 
-      {data.map((item, index) => (
+      {/* ФІЛЬТРИ */}
+      <div className={styles.filters}>
+        {categories.map((item) => (
+          <button
+            key={item.id}
+            onClick={() => handleFilterChange(item.slug)}
+            className={`${styles.filter} ${
+              activeCategory === item.slug ? styles.activeFilter : ""
+            }`}
+          >
+            {item.title}
+          </button>
+        ))}
+      </div>
+
+      {/* СПИСОК */}
+      {filteredData.map((item) => (
         <div
-          key={index}
+          key={item.id}
           className={styles.tabWrapper}
-          onClick={() => toggleTab(index)}
+          onClick={() => toggleTab(item.id)}
         >
           <div className={styles.tabTitleWrapper}>
             <p
-              className={`${styles.tabTitle} ${activeIndex === index ? styles.activeTab : ""}`}
-              style={{ paddingBottom: activeIndex == index ? "1.5vw" : "0px" }}
+              className={`${styles.tabTitle} ${
+                activeId === item.id ? styles.activeTab : ""
+              }`}
             >
               {item.title}
             </p>
-            <ArrowIcon open={activeIndex === index} />
+
+            <ArrowIcon open={activeId === item.id} />
           </div>
 
           <div
             className={`${styles.tabDescription} ${
-              activeIndex === index ? styles.open : ""
+              activeId === item.id ? styles.open : ""
             }`}
+            style={{
+              marginBottom: activeId === item.id ? "1.5vw" : "0px",
+            }}
           >
-            <p className={styles.tabContent}>{item.content}</p>
+            <p className={styles.tabContent}>{item.description}</p>
           </div>
         </div>
       ))}
