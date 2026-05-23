cdcb8cf8b7c0e2080cb5c42ac06a5a9f6a058415
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2026-05-24 00:12:28 +0300

    Fix dev cross origins

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  allowedDevOrigins: ["192.168.31.239"],
   images: {
     formats: ["image/avif", "image/webp"],
     domains: ["localhost"],
