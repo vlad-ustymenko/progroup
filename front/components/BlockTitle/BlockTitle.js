1eb54aa981735be3e0178657ac2c35aeefed7232
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2026-05-26 20:15:41 +0300

    Edit BlockTitle component

--- front/components/BlockTitle/BlockTitle.js
+++ front/components/BlockTitle/BlockTitle.js
@@ -3,11 +3,19 @@
 import { forwardRef } from "react";
 import styles from "./BlockTitle.module.css";
 
-const BlockTitle = forwardRef(function BlockTitle({ title }, ref) {
+const BlockTitle = forwardRef(function BlockTitle({ title, white }, ref) {
   return (
     <div className={styles.titleWrapper} ref={ref}>
-      <span className={styles.line}></span>
-      <span className={styles.title}>{title}</span>
+      <span
+        className={styles.line}
+        style={white ? { backgroundColor: "var(--accent)" } : {}}
+      ></span>
+      <span
+        className={styles.title}
+        style={white ? { color: "var(--accent)" } : {}}
+      >
+        {title}
+      </span>
     </div>
   );
 });
