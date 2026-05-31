06c0e65c09665b9b28d886d01c475727273c9f0a
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2026-05-31 23:25:24 +0300

    Footer: add data from Strapi

--- front/sections/Footer/Footer.jsx
+++ front/sections/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import ReactMarkdown from "react-markdown";
+import remarkBreaks from "remark-breaks";
 import React from "react";
 import FullLogo from "@/components/FullLogo/FullLogo";
 import {
@@ -8,7 +10,7 @@ import {
 } from "react-icons/bi";
 import styles from "./Footer.module.css";
 
-const Footer = () => {
+const Footer = ({ data }) => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
@@ -19,58 +21,46 @@ const Footer = () => {
             </a>
             <address className={styles.address}>
               <a
-                href="https://www.google.com.ua/maps/place/PRO-GROUP/@50.433745,30.3703793,13.5z/data=!4m6!3m5!1s0x40d4cbec77bcb20d:0xbf46f52bac4ab343!8m2!3d50.4367781!4d30.3561404!16s%2Fg%2F11y3l11_hb?hl=ru&entry=ttu&g_ep=EgoyMDI2MDUyNy4wIKXMDSoASAFQAw%3D%3D"
+                href={data.addressLink}
                 className={`${styles.text} ${styles.address}`}
                 target="_blank"
               >
-                Київська обл., 5км. від Києва с. Софіївська Борщагівка,
-                вул.Свободи, 1с
+                <ReactMarkdown remarkPlugins={[remarkBreaks]}>
+                  {data.address}
+                </ReactMarkdown>
               </a>
             </address>
-            <p className={styles.time}>Пн-Нд: 09:00-19:00</p>
+            <p className={styles.time}>{data.time}</p>
           </div>
           <div className={styles.mainInfo}>
             <div className={styles.developer}>
-              <p className={styles.title}>Девелопер</p>
-              <a href="#" className={styles.text}>
-                Про нас
-              </a>
-              <a href="#" className={styles.text}>
-                Засновник
-              </a>
-              <a href="#" className={styles.text}>
-                Проєкти
-              </a>
-              <a href="#" className={styles.text}>
-                Новини
-              </a>
+              <p className={styles.title}>{data.developerTitle}</p>
+              {data.developerInfo?.map((info, index) => (
+                <a key={index} href={info.link} className={styles.text}>
+                  {info.title}
+                </a>
+              ))}
             </div>
             <div className={styles.faq}>
-              <p className={styles.title}>Питання та відповіді</p>
-              <a href="#" className={styles.text}>
-                Питання та відповіді
-              </a>
-              <a href="#" className={styles.text}>
-                Контакти
-              </a>
-              <a href="#" className={styles.text}>
-                Політика конфіденційності
-              </a>
+              <p className={styles.title}>{data.faqTitle}</p>
+              {data.faqInfo?.map((info, index) => (
+                <a key={index} href={info.link} className={styles.text}>
+                  {info.title}
+                </a>
+              ))}
             </div>
           </div>
         </div>
         <div className={styles.copyrightWrapper}>
-          <p className={styles.copyright}>
-            © 2015 PRO-GROUP. Всі права захищені.
-          </p>
+          <p className={styles.copyright}>{data.copyright}</p>
           <div className={styles.socialsWrapper}>
-            <a href="#">
+            <a href={data.instagramLink}>
               <BiLogoInstagramAlt className={styles.social} />
             </a>
-            <a href="#">
+            <a href={data.facebookLink}>
               <BiLogoFacebookSquare className={styles.social} />
             </a>
-            <a href="#">
+            <a href={data.telegramLink}>
               <BiLogoTelegram className={styles.social} />
             </a>
           </div>
