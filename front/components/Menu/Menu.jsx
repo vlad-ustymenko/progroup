754dbfcdb0f739c7f565a33ad0822513ab30ff86
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2026-06-06 00:22:10 +0300

    Menu: add strapi data

--- front/components/Menu/Menu.jsx
+++ front/components/Menu/Menu.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { useMenuContext } from "../../Context/MenuContext";
 import styles from "./Menu.module.css";
 
-const Menu = () => {
+const Menu = ({ data }) => {
   const { activeMenu, setActiveMenu } = useMenuContext();
   useEffect(() => {
     if (activeMenu) {
@@ -16,41 +16,13 @@ const Menu = () => {
   return (
     <nav className={activeMenu ? `${styles.nav} ${styles.active}` : styles.nav}>
       <ul className={styles.navList}>
-        <li onClick={() => setActiveMenu(false)}>
-          <a href="#guarantees" className={styles.navLink}>
-            wefwe
-          </a>
-        </li>
-        <li onClick={() => setActiveMenu(false)}>
-          <a href="#advantages" className={styles.navLink}>
-            wefwe
-          </a>
-        </li>
-        <li onClick={() => setActiveMenu(false)}>
-          <a href="#prices" className={styles.navLink}>
-            werf
-          </a>
-        </li>
-        <li onClick={() => setActiveMenu(false)}>
-          <a href="#about" className={styles.navLink}>
-            wefwe
-          </a>
-        </li>
-        <li onClick={() => setActiveMenu(false)}>
-          <a href="#projects" className={styles.navLink}>
-            wefwe
-          </a>
-        </li>
-        <li onClick={() => setActiveMenu(false)}>
-          <a href="#services" className={styles.navLink}>
-            wefwe
-          </a>
-        </li>
-        <li onClick={() => setActiveMenu(false)}>
-          <a href="#contacts" className={styles.navLink}>
-            wefwe
-          </a>
-        </li>
+        {data.map((item) => (
+          <li key={item.id} onClick={() => setActiveMenu(false)}>
+            <a href={item.link} className={styles.navLink}>
+              {item.title}
+            </a>
+          </li>
+        ))}
       </ul>
       <a href="tel:+38 (095) 319-57-58" className={styles.telephone}>
         +38 (095) 319-57-58
