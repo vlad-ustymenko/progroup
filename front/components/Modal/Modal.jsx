5af7be3f58f8af7af6ca0e4e6419edda30b0d0e6
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2026-06-09 23:59:12 +0300

    Add Modal for news and projects

--- front/components/Modal/Modal.jsx
+++ front/components/Modal/Modal.jsx
@@ -1,22 +1,45 @@
 "use client";
 import { useModal } from "@/Context/ModalContext";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import Image from "next/image";
 import styles from "./Modal.module.css";
+import ReactMarkdown from "react-markdown";
+import remarkBreaks from "remark-breaks";
 import Loader from "../Loader/Loader";
+import { IoClose } from "react-icons/io5";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 
 const Modal = () => {
-  const { isOpen, data, closeModal, sending, isform } = useModal();
+  const cardSwiperRef = useRef({});
+  const { isOpen, data, closeModal, sending, isform, isProject } = useModal();
   useEffect(() => {
     document.documentElement.style.overflow = isOpen ? "hidden" : "";
-  }, [isOpen]);
+    const handleKeyDown = (e) => {
+      if (e.key === "Escape") {
+        closeModal();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isOpen, closeModal]);
   if (!isOpen || !data) return null;
 
   console.log(data);
 
   if (isform) {
     return (
-      <div className={styles.modal} onClick={closeModal}>
+      <div
+        className={sending ? styles.sending : styles.modal}
+        onClick={closeModal}
+      >
         {sending ? (
           <Loader></Loader>
         ) : (
@@ -28,9 +51,8 @@ const Modal = () => {
               <h2 className={styles.title}>qwd</h2>
               <p className={styles.description}>qwd</p>
             </div>
-            <button className={styles.closeButton} onClick={closeModal}>
-              Закрити
-            </button>
+
+            <IoClose className={styles.closeButton} onClick={closeModal} />
           </div>
         )}
       </div>
@@ -41,20 +63,72 @@ const Modal = () => {
     <div className={styles.modal} onClick={closeModal}>
       <div className={styles.wrapper} onClick={(e) => e.stopPropagation()}>
         <div className={styles.imageWrapper}>
-          {/* <Image
-            className={styles.image}
-            src={`${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}${data.image.url}`}
-            alt=""
-            fill
-          ></Image> */}
+          {!isProject ? (
+            <Image
+              className={styles.image}
+              src={`${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}${data.image.url}`}
+              alt=""
+              fill
+            ></Image>
+          ) : (
+            <Swiper
+              modules={[Pagination]}
+              slidesPerView={1}
+              loop={true}
+              speed={800}
+              onSwiper={(swiper) => {
+                cardSwiperRef.current[data.id] = swiper;
+              }}
+              className={styles.swiperCard}
+            >
+              {data.images.map((image) => (
+                <SwiperSlide key={image.documentId}>
+                  <Image
+                    className={styles.image}
+                    src={`${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}${image.url}`}
+                    alt=""
+                    fill
+                  />
+                </SwiperSlide>
+              ))}
+
+              <button
+                className={`${styles.cardNav} ${styles.cardPrev}`}
+                onClick={() => cardSwiperRef.current[data.id]?.slidePrev()}
+              >
+                <FaChevronLeft
+                  className={`${styles.icon} ${styles.iconLeft}`}
+                />
+              </button>
+
+              <button
+                className={`${styles.cardNav} ${styles.cardNext}`}
+                onClick={() => cardSwiperRef.current[data.id]?.slideNext()}
+              >
+                <FaChevronRight
+                  className={`${styles.icon} ${styles.iconRight}`}
+                />
+              </button>
+            </Swiper>
+          )}
         </div>
         <div className={styles.modalContent}>
           <h2 className={styles.title}>{data.title}</h2>
-          <p className={styles.description}>{data.descriptionMore}</p>
+          <ReactMarkdown
+            remarkPlugins={[remarkBreaks]}
+            components={{
+              p: ({ children }) => (
+                <p className={styles.description}>{children}</p>
+              ),
+              strong: ({ children }) => (
+                <span className={styles.strong}>{children}</span>
+              ),
+            }}
+          >
+            {data.descriptionMore}
+          </ReactMarkdown>
         </div>
-        <button className={styles.closeButton} onClick={closeModal}>
-          Закрити
-        </button>
+        <IoClose className={styles.closeButton} onClick={closeModal} />
       </div>
     </div>
   );
